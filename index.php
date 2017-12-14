10aba20ecce8b1919541b6d9ebc21b3f95e660a5
codenannu <nandkishor_11@live.com>
2017-12-14 13:08:11 +0530

    commit after updating a file in vim editor.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	    <title>click</title>
 	    
-	    <!-- Latest compiled and minified CSS -->
+	    <!-- Latest compiled and minified css -->
 	    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
 		
 		<!-- jQuery library -->
